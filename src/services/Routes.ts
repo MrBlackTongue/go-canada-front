e00556c2eb099547df842d48aabdef65b43fa85a
MrBlackTongue <Lana-14-92@mail.ru>
2023-03-07 19:56:09 +0400

    add types and routes

---
+++ src/services/Routes.ts
@@ -0,0 +1,5 @@
+export const URL = 'http://localhost:8080/api/'
+
+export const ARTICLE = 'article'
+
+
---
+++ src/types/ArticleTypes.ts
@@ -0,0 +1,6 @@
+export type ArticleTypes = {
+  id?: number;
+  title?: string;
+  content?: string;
+  createdAt?: string;
+}
