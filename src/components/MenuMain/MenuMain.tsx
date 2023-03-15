4604f325a500db07fac3660fb180681b131e78c2
AlAlon369 <alalon211090@gmail.com>
2023-03-15 17:33:17 +0300

    Убрал ненужные страницы из меню

--- src/components/MenuMain/MenuMain.tsx
+++ src/components/MenuMain/MenuMain.tsx
@@ -2,8 +2,6 @@ import React, {useState} from 'react';
 import {
   AppstoreOutlined,
   MailOutlined,
-  SettingOutlined,
-  TeamOutlined,
 } from '@ant-design/icons';
 import type {MenuProps} from 'antd';
 import {Menu} from 'antd';
@@ -12,7 +10,13 @@ import {Link} from "react-router-dom";
 const items = [
   {
     label: 'Главная',
-    key: 'mail',
+    key: 'main',
+    icon: <Link to="/"><MailOutlined/></Link>,
+    link: '/',
+  },
+  {
+    label: 'Все статьи',
+    key: 'all-article',
     icon: <Link to="/all-article"><MailOutlined/></Link>,
     link: '/all-article',
   },
@@ -22,81 +26,19 @@ const items = [
     icon: <Link to="/add-article"><AppstoreOutlined/></Link>,
     link: '/add-article',
   },
-  {
-    label: 'Иммиграция',
-    key: 'article',
-    icon: <Link to=""><MailOutlined/></Link>,
-    link: '',
-  },
-  {
-    label: 'Виза',
-    key: 'SubMenu',
-    icon: <SettingOutlined/>,
-    children: [
-      {
-        type: 'group',
-        label: 'Item 1',
-        children: [
-          {
-            label: 'Option 1',
-            key: 'setting:1',
-          },
-          {
-            label: 'Option 2',
-            key: 'setting:2',
-          },
-        ],
-      },
-      {
-        type: 'group',
-        label: 'Item 2',
-        children: [
-          {
-            label: 'Option 3',
-            key: 'setting:3',
-          },
-          {
-            label: 'Option 4',
-            key: 'setting:4',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: 'Работа',
-    key: 'SubMenu2',
-    icon: <TeamOutlined/>,
-    children: [
-      {
-        type: 'group',
-        label: 'Item 1',
-        children: [
-          {
-            label: 'Option 1',
-            key: 'setting:5',
-          },
-          {
-            label: 'Option 2',
-            key: 'setting:6',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    label: (
-      <a href="https://in-ca.ru/express-entry/16-immigration-to-canada-express-entry.html" target="_blank"
-         rel="noopener noreferrer">
-        Ссылка на источник
-      </a>
-    ),
-    key: 'alipay',
-  },
+  // {
+  //   label: (
+  //     <a href="https://in-ca.ru/express-entry/16-immigration-to-canada-express-entry.html" target="_blank"
+  //        rel="noopener noreferrer">
+  //       Ссылка на источник
+  //     </a>
+  //   ),
+  //   key: 'alipay',
+  // },
 ];
 
 const MenuMain: React.FC = () => {
-  const [current, setCurrent] = useState('mail');
+  const [current, setCurrent] = useState('main');
 
   const onClick: MenuProps['onClick'] = (e) => {
     // console.log('click ', e);
