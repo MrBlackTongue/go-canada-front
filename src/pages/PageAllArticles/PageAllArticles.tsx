a94a3fac92e5d67672fb44bb47c13d0835a643af
MrBlackTongue <Lana-14-92@mail.ru>
2023-03-20 17:53:19 +0400

    add Spin

--- src/pages/PageAllArticles/PageAllArticles.tsx
+++ src/pages/PageAllArticles/PageAllArticles.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import 'react-quill/dist/quill.snow.css';
-import {Button, Card, Typography, Space, Popconfirm, FloatButton} from "antd";
+import {Button, Card, Typography, Space, Popconfirm, FloatButton, Spin} from "antd";
 import {getAllArticles, deleteArticleById} from "../../services";
 import {ArticleType} from "../../types";
 import {ZoomInOutlined, DeleteOutlined, EditOutlined} from '@ant-design/icons';
@@ -35,53 +35,59 @@ const PageAllArticles: React.FC = () => {
         <div className='centerTitle'>
           <Title level={3}>Все статьи</Title>
         </div>
-        <Space direction="vertical" style={{width: '100%', gap: '30px'}}>
-          {articles?.map((article) => (
-            <Card
-              hoverable
-              key={article.id}
-              title={article.title}
-              style={{width: '100%'}}
-              extra={
-                <Space>
-                  <Link to={`/article/${article.id}`}>
-                    <Button
-                      className='blueButton'
-                      icon={<ZoomInOutlined/>}>
-                      Просмотреть
-                    </Button>
-                  </Link>
-                  <Link to={`/edit-article/${article.id}`}>
-                    <Button
-                      type="dashed"
-                      icon={<EditOutlined/>}
-                      className='greenButton'
-                    >
-                      Редактировать
-                    </Button>
-                  </Link>
-                  {article.id && (
-                    <Popconfirm
-                      title="Вы уверены, что хотите удалить эту статью?"
-                      onConfirm={() => handleDelete(article.id)}
-                      okText="Да"
-                      cancelText="Нет"
-                    >
-                      <Button
-                        danger icon={<DeleteOutlined/>}>
-                        Удалить
-                      </Button>
-                    </Popconfirm>
+        {articles ? (
+          <>
+            <Space direction="vertical" style={{width: '100%', gap: '30px'}}>
+              {articles?.map((article) => (
+                <Card
+                  hoverable
+                  key={article.id}
+                  title={article.title}
+                  style={{width: '100%'}}
+                  extra={
+                    <Space>
+                      <Link to={`/article/${article.id}`}>
+                        <Button
+                          className='blueButton'
+                          icon={<ZoomInOutlined/>}>
+                          Просмотреть
+                        </Button>
+                      </Link>
+                      <Link to={`/edit-article/${article.id}`}>
+                        <Button
+                          type="dashed"
+                          icon={<EditOutlined/>}
+                          className='greenButton'
+                        >
+                          Редактировать
+                        </Button>
+                      </Link>
+                      {article.id && (
+                        <Popconfirm
+                          title="Вы уверены, что хотите удалить эту статью?"
+                          onConfirm={() => handleDelete(article.id)}
+                          okText="Да"
+                          cancelText="Нет"
+                        >
+                          <Button
+                            danger icon={<DeleteOutlined/>}>
+                            Удалить
+                          </Button>
+                        </Popconfirm>
+                      )}
+                    </Space>
+                  }
+                >
+                  {article.content && (
+                    <div dangerouslySetInnerHTML={{__html: article.content}}/>
                   )}
-                </Space>
-              }
-            >
-              {article.content && (
-                <div dangerouslySetInnerHTML={{__html: article.content}}/>
-              )}
-            </Card>
-          ))}
-        </Space>
+                </Card>
+              ))}
+            </Space>
+          </>
+        ) : (
+          <Spin size="large"/>
+        )}
       </div>
       <FloatButton.BackTop/>
     </Card>
--- src/pages/PageEditArticle/PageEditArticle.tsx
+++ src/pages/PageEditArticle/PageEditArticle.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useParams, useNavigate} from 'react-router-dom';
-import {Card, Typography, Input, Button, FloatButton} from 'antd';
+import {Card, Typography, Input, Button, FloatButton, Spin} from 'antd';
 import {getArticleById, putUpdateArticle} from '../../services';
 import {ArticleType} from '../../types';
 import ReactQuill from "react-quill";
@@ -44,40 +44,46 @@ export const PageEditArticle: React.FC = () => {
 
   return (
     <Card style={{width: '70%', margin: '0 auto'}}>
-      <div style={{display: 'grid'}}>
-        <div className="centerTitle">
-          <Title level={3}>Редактировать статью</Title>
-        </div>
-      </div>
-      <div>
-        <Input
-          value={article?.title}
-          style={{width: 600, marginBottom: '1em'}}
-          placeholder="Название статьи"
-          onChange={handleTitleChange}
-        />
-        <ReactQuill
-          value={article?.content}
-          onChange={handleChange}
-          placeholder="Текст статьи..."
-          modules={{
-            toolbar: [
-              [{'header': [1, 2, false]}],
-              ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-              [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-              ['link', 'image'],
-              ['clean']
-            ],
-          }}
-          formats={[
-            'header',
-            'bold', 'italic', 'underline', 'strike', 'blockquote',
-            'list', 'bullet', 'indent',
-            'link', 'image',
-          ]}
-        />
-        <Button onClick={updateArticle} style={{marginTop: 10}}>Сохранить</Button>
-      </div>
+      {article ? (
+        <>
+          <div style={{display: 'grid'}}>
+            <div className="centerTitle">
+              <Title level={3}>Редактировать статью</Title>
+            </div>
+          </div>
+          <div>
+            <Input
+              value={article?.title}
+              style={{width: 600, marginBottom: '1em'}}
+              placeholder="Название статьи"
+              onChange={handleTitleChange}
+            />
+            <ReactQuill
+              value={article?.content}
+              onChange={handleChange}
+              placeholder="Текст статьи..."
+              modules={{
+                toolbar: [
+                  [{'header': [1, 2, false]}],
+                  ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+                  [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+                  ['link', 'image'],
+                  ['clean']
+                ],
+              }}
+              formats={[
+                'header',
+                'bold', 'italic', 'underline', 'strike', 'blockquote',
+                'list', 'bullet', 'indent',
+                'link', 'image',
+              ]}
+            />
+            <Button onClick={updateArticle} style={{marginTop: 10}}>Сохранить</Button>
+          </div>
+        </>
+      ) : (
+        <Spin size="large"/>
+      )}
       <FloatButton.BackTop/>
     </Card>
   );
