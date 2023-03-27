2660aea1a077f81917a2eef82935b0a2aab0b2db
MrBlackTongue <Lana-14-92@mail.ru>
2023-03-27 13:35:28 +0400

    layout at work

--- src/App.tsx
+++ src/App.tsx
@@ -9,28 +9,31 @@ const {Header, Content, Footer} = Layout;
 const App: React.FC = () => {
 
   return (
-    <Layout>
-      <Header style={{position: 'sticky', top: 0, zIndex: 1, width: '100%', background: "white"}}>
+    <Layout className='layout-page'>
+      <Header className='header-page'>
         <img
           src={`${process.env.PUBLIC_URL}/logo.png`}
           alt="Logo"
+          className='logo-page'
           style={{
             float: 'left',
             width: 160,
             height: 67,
-          }}/>
+          }}
+        />
         <MenuMain/>
       </Header>
       <Layout className="site-layout">
-        <Content className="site-layout" style={{padding: '0 50px'}}>
-          <Breadcrumb style={{margin: '16px 0'}}>
-            {/*<Breadcrumb.Item>Главная</Breadcrumb.Item>*/}
-            {/*<Breadcrumb.Item>Все статьи</Breadcrumb.Item>*/}
+        <Content className="content-page">
+          <Breadcrumb className='breadcrumb-page' style={{margin: '16px 0'}}>
+            <Breadcrumb.Item>Главная</Breadcrumb.Item>
+            <Breadcrumb.Item>Express Entry</Breadcrumb.Item>
+            <Breadcrumb.Item>Educational credential assessment</Breadcrumb.Item>
           </Breadcrumb>
           <AppRoutes/>
         </Content>
       </Layout>
-      <Footer style={{textAlign: 'center'}}>Если вы еще не в Канаде, тогда мы идем к вам!</Footer>
+      <Footer className='footer-page'>© 2023 gocanada.pro</Footer>
     </Layout>
   )
 }
--- src/services/Routes.ts
+++ src/services/Routes.ts
@@ -1,5 +1,5 @@
-// export const URL = 'https://gocanada.pro/api'  // для прода
-export const URL = 'http://localhost:8090/api'    // для локальной работы
+export const URL = 'https://gocanada.pro/api'  // для прода
+// export const URL = 'http://localhost:8090/api'    // для локальной работы
 
 export const ARTICLE = '/article'
 export const ALL = '/all'
